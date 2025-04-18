a6217fd30e63b12aabd085ba8b8ca479d1e2e7bf
vivekPandeyDev <codewithvivekpandey@gmail.com>
2025-04-18 19:46:07 +0530

    basic setup for book service

---
+++ book-service/src/main/java/org/loop/troop/book/BookServcieApplication.java
@@ -0,0 +1,13 @@
+package org.loop.troop.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookServcieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookServcieApplication.class, args);
+	}
+
+}
---
+++ book-service/src/test/java/org/loop/troop/book/BookServcieApplicationTests.java
@@ -0,0 +1,13 @@
+package org.loop.troop.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookServcieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
