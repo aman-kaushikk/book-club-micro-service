a82895e4e547ea8bd3658830c9b524599053842a
vivekPandeyDev <codewithvivekpandey@gmail.com>
2025-05-17 21:25:10 +0530

    feature/k8s: test case failed resolved

--- club-service/src/test/java/org/loop/troop/club/ClubServiceApplicationTests.java
+++ club-service/src/test/java/org/loop/troop/club/ClubServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package org.loop.troop.book;
+package org.loop.troop.club;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class BookServcieApplicationTests {
+@SpringBootTest(classes = ClubServiceApplication.class)
+class ClubServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
